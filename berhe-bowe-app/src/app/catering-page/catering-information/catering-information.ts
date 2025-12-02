afbf701b204aaecde8b5ee7d408d6800a8548544
Elizabeth Bowe <elizabethbowe@yahoo.com>
2025-12-02 12:39:48 -0500

    initial catering form updates, still have to add restrictions and more input validation

--- berhe-bowe-app/src/app/catering-page/catering-information/catering-information.ts
+++ berhe-bowe-app/src/app/catering-page/catering-information/catering-information.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, Validators, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-catering-information',
@@ -11,15 +11,42 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
 export class CateringInformation {
   constructor ( private router: Router) {}
 
-  name = new FormControl('');
-  phone = new FormControl('');
-  email = new FormControl('');
-  people = new FormControl('');
-  company = new FormControl('');
-  date = new FormControl('');
-  time = new FormControl('');
-  occasion = new FormControl('');
-  description = new FormControl('');
+  cateringForm = new FormGroup ( {
+      name : new FormControl('', [
+        Validators.required,
+        Validators.pattern ( /^[a-zA-Z\s]+$/ )
+      ]),
+
+      phone : new FormControl('', [
+        Validators.required,
+        Validators.pattern( /^[0-9\-()+\s]{7,20}$/ )
+      ]),
+
+      email : new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+
+      people : new FormControl('', [
+        Validators.required,
+        Validators.pattern( /^[0-9]+$/ )
+      ]),
+
+      company : new FormControl('', [ ] ),
+
+      date : new FormControl('', [
+        Validators.required,
+        Validators.pattern( /^\d{2}-\d{2}-\d{4}$/)
+      ]),
+
+      time : new FormControl('', [
+        Validators.required,
+        Validators.pattern( /^\d{1,2}:\d{2}$/ )
+      ]),
+
+      occasion : new FormControl(''),
+      description : new FormControl('')
+  });
 
   submitInformation() {
     this.router.navigate ( [ '../catering-order' ] );
